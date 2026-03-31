063fc55753d347bdab23bf6df778b5432d1e01e6
Aniketh B <aniketh.balendran@gmail.com>
2026-03-31 16:54:57 -0500

    fix cleanup

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -92,24 +92,6 @@ const fetchGoogleAccessTokenFromEnv = async () => {
   return tokenResponse.access_token;
 };
 
-// #region agent log
-const agentLog = (location, message, data, hypothesisId) => {
-  const payload = {
-    sessionId: 'e0ac77',
-    location,
-    message,
-    data,
-    timestamp: Date.now(),
-    hypothesisId
-  };
-  fetch('http://127.0.0.1:7382/ingest/eee79fb1-b801-4baa-bd86-e0d193626bd1', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'X-Debug-Session-Id': 'e0ac77' },
-    body: JSON.stringify(payload)
-  }).catch(() => {});
-};
-// #endregion
-
 const initFirebase = () => {
   if (firebaseInitialized) {
     return admin;
@@ -123,19 +105,6 @@ const initFirebase = () => {
   } = process.env;
 
   if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY || !FIREBASE_STORAGE_BUCKET) {
-    // #region agent log
-    agentLog(
-      'storageService.js:initFirebase:missingEnv',
-      'firebase env incomplete',
-      {
-        hasProjectId: !!FIREBASE_PROJECT_ID,
-        hasClientEmail: !!FIREBASE_CLIENT_EMAIL,
-        hasPrivateKey: !!FIREBASE_PRIVATE_KEY,
-        hasBucket: !!FIREBASE_STORAGE_BUCKET
-      },
-      'H1'
-    );
-    // #endregion
     console.warn(
       'Firebase Storage is not fully configured. ' +
       'Required env vars: FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY, FIREBASE_STORAGE_BUCKET'
@@ -221,28 +190,7 @@ const uploadBufferViaGcsRest = async (bucketName, objectPath, buffer, contentTyp
  * Expects `file` to be a Multer file object using memoryStorage.
  */
 const uploadTaskFile = async (taskId, file) => {
-  // #region agent log
-  agentLog(
-    'storageService.js:uploadTaskFile:entry',
-    'upload start',
-    {
-      hasBuffer: !!(file && file.buffer),
-      bufferLen: file && file.buffer ? file.buffer.length : 0,
-      mimetype: file && file.mimetype
-    },
-    'H5'
-  );
-  // #endregion
-
   const adminInstance = initFirebase();
-  // #region agent log
-  agentLog(
-    'storageService.js:uploadTaskFile:afterInit',
-    'firebase init',
-    { hasAdmin: !!adminInstance },
-    'H1'
-  );
-  // #endregion
   if (!adminInstance) {
     throw new Error('Firebase is not configured');
   }
@@ -258,40 +206,8 @@ const uploadTaskFile = async (taskId, file) => {
 
   const fileRef = bucket.file(filePath);
 
-  try {
-    await uploadBufferViaGcsRest(bucket.name, filePath, file.buffer, file.mimetype);
-    // #region agent log
-    agentLog('storageService.js:uploadTaskFile:afterSave', 'gcs REST upload ok', { filePath }, 'H2');
-    // #endregion
-  } catch (saveErr) {
-    // #region agent log
-    agentLog(
-      'storageService.js:uploadTaskFile:saveErr',
-      'gcs REST upload failed',
-      { msg: saveErr && saveErr.message, code: saveErr && saveErr.code },
-      'H2'
-    );
-    // #endregion
-    throw saveErr;
-  }
-
-  try {
-    // Make file publicly accessible
-    await fileRef.makePublic();
-    // #region agent log
-    agentLog('storageService.js:uploadTaskFile:afterMakePublic', 'makePublic ok', {}, 'H3');
-    // #endregion
-  } catch (pubErr) {
-    // #region agent log
-    agentLog(
-      'storageService.js:uploadTaskFile:makePublicErr',
-      'makePublic failed',
-      { msg: pubErr && pubErr.message, code: pubErr && pubErr.code },
-      'H3'
-    );
-    // #endregion
-    throw pubErr;
-  }
+  await uploadBufferViaGcsRest(bucket.name, filePath, file.buffer, file.mimetype);
+  await fileRef.makePublic();
 
   const publicUrl = `https://storage.googleapis.com/${bucket.name}/${filePath}`;
   return {
